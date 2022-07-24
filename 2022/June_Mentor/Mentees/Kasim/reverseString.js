4667ec6ae89a7a90f6b99215986663f3ffdb257d
salman3009 <salman444g@gmail.com>
2022-07-24 12:02:28 +0530

    updated code

---
+++ 2022/June_Mentor/Mentees/Kasim/reverseString.js
@@ -0,0 +1,8 @@
+var input = "salman";
+console.log(input);
+var output = input.split('');
+console.log(output);
+output = output.reverse();
+console.log(output);
+output = output.join('');
+console.log(output);
