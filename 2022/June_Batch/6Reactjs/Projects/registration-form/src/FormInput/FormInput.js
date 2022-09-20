446a4d809190588a9420dbb86b7e8d15fa2f3794
salman3009 <salman444g@gmail.com>
2022-09-20 22:24:44 +0530

    code

--- 2022/June_Batch/6Reactjs/Projects/registration-form/src/FormInput/FormInput.js
+++ 2022/June_Batch/6Reactjs/Projects/registration-form/src/FormInput/FormInput.js
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './FormInput.css';
 import {alphaNumericValidation,emailValidation,passwordValidation,bioValidation,telephoneValidation} from '../Helper/validation';
 
 const FormInput=()=>{
+
+   const [getGlobal,setGlobal]=useState(false);
+
    const [getForm,setForm]=useState({
     firstName:'',
     lastName:'',
@@ -21,12 +24,28 @@ const FormInput=()=>{
     bio:false
    });
 
+   useEffect(()=>{
+    formValidationCheck();
+   },[getForm])
+
    const onChangeHandler=(event)=>{
-      setForm({...getForm,[event.target.name]:event.target.value});
+      setForm({...getForm,[event.target.name]:event.target.value}); 
+   }
+
+   const formValidationCheck=()=>{
+    setFormValidation({
+        firstName:alphaNumericValidation(getForm.firstName),
+        lastName:alphaNumericValidation(getForm.lastName),
+        email:emailValidation(getForm.email),
+        password:passwordValidation(getForm.password),
+        telephone:telephoneValidation(getForm.telephone),
+        bio:bioValidation(getForm.bio)  
+    });   
    }
 
    const onSubmit=()=>{
-     
+    formValidationCheck();
+    setGlobal(true);
    }
 
      return (<div>
@@ -35,45 +54,45 @@ const FormInput=()=>{
               First Name
             </label>
             <input type="text" value={getForm.firstName} onChange={onChangeHandler} name="firstName"/>
-           {getFormValidation.firstName && <div className="danger">First Name must be alphanumeric and contain 3 - 16 characters</div>} 
+           {getGlobal && getFormValidation.firstName && <div className="danger">First Name must be alphanumeric and contain 3 - 16 characters</div>} 
         </div>
         <div className="form-group">
             <label>
               Last Name
             </label>
             <input type="text" value={getForm.lastName} onChange={onChangeHandler}  name="lastName"/>
-            {getFormValidation.lastName && <div className="danger">Last Name must be alphanumeric and contain 3 - 16 characters</div>}
+            {getGlobal && getFormValidation.lastName && <div className="danger">Last Name must be alphanumeric and contain 3 - 16 characters</div>}
         </div>
         <div className="form-group">
             <label>
               Email
             </label>
             <input type="text" value={getForm.email} onChange={onChangeHandler}  name="email"/>
-            {getFormValidation.email && <div className="danger">Email must be valid address,e.g:akash@gamil.com</div>}
+            {getGlobal && getFormValidation.email && <div className="danger">Email must be valid address,e.g:akash@gamil.com</div>}
         </div>
         <div className="form-group">
             <label>
               Password
             </label>
             <input type="text" value={getForm.password} onChange={onChangeHandler}  name="password"/>
-            {getFormValidation.password && <div className="danger">Password must be alphanumeric(@,_and - are also allowed) and between 6-20 characters</div>}
+            {getGlobal && getFormValidation.password && <div className="danger">Password must be alphanumeric(@,_and - are also allowed) and between 6-20 characters</div>}
         </div>
         <div className="form-group">
             <label>
               Telephone
             </label>
             <input type="text" value={getForm.telephone} onChange={onChangeHandler}   name="telephone"/>
-            {getFormValidation.telephone && <div className="danger">A valid telphone number (11 digits and 333-333-3333)</div>} 
+            {getGlobal && getFormValidation.telephone && <div className="danger">A valid telphone number (11 digits and 333-333-3333)</div>} 
         </div>
         <div className="form-group">
             <label>
               Your Bio
             </label>
             <input type="text" value={getForm.bio} onChange={onChangeHandler}   name="bio"/>
-            {getFormValidation.bio && <div className="danger">Bio must contain only lowercase letters,underscores,hyphens and be 8-50 characters</div>}
+            {getGlobal && getFormValidation.bio && <div className="danger">Bio must contain only lowercase letters,underscores,hyphens and be 8-50 characters</div>}
         </div>
         <div className="form-group">
-         <button>Submit</button>
+         <button onClick={onSubmit}>Submit</button>
         </div>
      </div>)
 }
