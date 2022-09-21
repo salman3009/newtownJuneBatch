44af3d038c7c1bab729307e37bb512733e4b9113
salman3009 <salman444g@gmail.com>
2022-09-21 21:20:16 +0530

    code

---
+++ 2022/June_Batch/6Reactjs/Projects/image-list/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ImageList from './ImageList/ImageList';
+function App() {
+  return (
+    <div className="App">
+      <ImageList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2022/June_Batch/6Reactjs/Projects/image-list/src/ImageList/ImageList.js
@@ -0,0 +1,10 @@
+import './ImageList.css';
+import image from '../assets/image/1car.png';
+
+const ImageList = ()=>{
+   return (<div className="image-box">
+    <img src={image}/>
+   </div>)
+}
+
+export default ImageList;
