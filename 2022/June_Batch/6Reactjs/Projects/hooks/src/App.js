04e50b26898f51b1cbc4ea3b4bddb1fbcdeee163
salman3009 <salman444g@gmail.com>
2022-09-29 21:58:00 +0530

    code

--- 2022/June_Batch/6Reactjs/Projects/hooks/src/App.js
+++ 2022/June_Batch/6Reactjs/Projects/hooks/src/App.js
@@ -1,45 +1,62 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import List from './List';
 
+const App = () => {
 
-const App=()=>{
-
-  const [getCount,setCount]=useState(0);
+  const [getCount, setCount] = useState(0);
+  const [getFlag, setFlag] = useState(true);
 
   //Only one time it is going to call
   //similar to componentDidMount
-  useEffect(()=>{
-    alert("only one time it is going to call");
+  useEffect(() => {
+    // alert("only one time it is going to call");
     setCount(1);
-  },[])
+  }, [])
 
   //It is going to call everytime when there is change in state
   //similar to componentDidUpdate
-  useEffect(()=>{
-    alert("It is going to call everytime updates happens");
-    if(getCount>=5){
+  useEffect(() => {
+    // alert("It is going to call everytime updates happens");
+    if (getCount >= 5) {
       setCount(1);
     }
   })
-  
-const onIncrementHandler=()=>{
-  setCount(getCount+1);
 
-}
+  const onIncrementHandler = () => {
+    setCount(getCount + 1);
 
-const onDecrementHandler=()=>{
-  setCount(getCount-1);
-}
+  }
 
-  return (<div className="container">
-             <div>
-              <input type="text" value={getCount} readOnly/>
-             </div>
-             <div>
-              <button onClick={onIncrementHandler}>Increment</button>
-              <button onClick={onDecrementHandler}>Decrement</button>
-             </div>
-  </div>)
+  const onDecrementHandler = () => {
+    setCount(getCount - 1);
+  }
+
+  const flagHandler = () => {
+    setFlag(!getFlag);
+  }
+
+  return (<>
+    <div className="container">
+      <div>
+        <input type="text" value={getCount} readOnly />
+      </div>
+      <div>
+        <button onClick={onIncrementHandler}>Increment</button>
+        <button onClick={onDecrementHandler}>Decrement</button>
+      </div>
+
+    </div>
+    <div className="container">
+      <div>
+        <button onClick={flagHandler}>Show/Hide</button>
+      </div>
+      <div>
+        {getFlag && <List />}
+      </div>
+    </div>
+
+  </>)
 
 }
 export default App;
---
+++ 2022/June_Batch/6Reactjs/Projects/hooks/src/List.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+
+const List=()=>{
+
+    //similar to component willunmount
+    //whenver you remove this component form DOM
+   
+
+    useEffect(()=>{
+        const details = setInterval(()=>{
+            console.log("interval is calling");
+        },2000);
+        return ()=>clearInterval(details);
+    },[])
+
+    return <div>
+        <ul>
+            <li>Pen</li>
+            <li>Pencil</li>
+            <li>Sharpner</li>
+        </ul>
+    </div>
+}
+
+export default List;
