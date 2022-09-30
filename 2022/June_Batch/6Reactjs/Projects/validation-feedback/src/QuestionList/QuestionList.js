367ceeaf664312f4cf667e4afd65331263c29c4f
salman3009 <salman444g@gmail.com>
2022-09-30 22:29:35 +0530

    code

--- 2022/June_Batch/6Reactjs/Projects/validation-feedback/src/QuestionList/QuestionList.js
+++ 2022/June_Batch/6Reactjs/Projects/validation-feedback/src/QuestionList/QuestionList.js
@@ -1,22 +1,74 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './QuestionList.css';
 import axios from 'axios';
 
 const QuestionList = () => {
 
+    const [getList, setList] = useState([]);
+    const [getAnswer,setAnswer]=useState([]);
+
     useEffect(() => {
         getInitialDetails();
     }, [])
 
+    useEffect(()=>{
+       let list=[];
+        if(getList.length){
+            for(let obj of getList){
+                list.push({
+                    question:obj.question,
+                    id:obj.id,
+                    value:''
+                })
+            }
+            setAnswer([...list]);
+        }
+    },[getList]);
+
+    useEffect(()=>{
+       console.log(getAnswer);
+    },[getAnswer])
+
     const getInitialDetails = async () => {
         try {
             const api = await axios.get("http://localhost:3000/feedbackquestion");
-            console.log(api.data);
+            //console.log(api.data);
+            setList([...api.data]);
         } catch (err) {
-            console.log(err);
+           // console.log(err);
         }
     }
 
-    return (<div>Hello</div>)
+    const onSelectionIndex=(id,data)=>{
+        let list = getAnswer;
+        let index=-1;
+           for(let i=0 ;i<list.length;i++){
+              if(list[i].id === id){
+                  index=i;
+                  break;
+              }
+           }
+        list[index].value = data;
+        setAnswer([...list]);
+           console.log(index,data);
+    }
+
+    return (<div className='container'>
+        {getList.map((obj, index) => {
+            return (<div className="heading" key={index}>
+                <h3>{obj.question}</h3>
+                {obj.options.map((data, i) => {
+                    return (
+                        <div key={i}>
+                            <input onClick={()=>onSelectionIndex(obj.id,data)} type="radio" value={data} name={index} />
+                            {data}
+                        </div>
+                    )
+                })}
+
+            </div>)
+        })}
+
+    </div>)
 }
 export default QuestionList;
