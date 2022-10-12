0588e1fe829c7cca53b2ec78b8f3e03aa673d8bf
salman3009 <salman444g@gmail.com>
2022-10-12 19:35:45 +0530

    code

--- 2022/June_Batch/6Reactjs/Projects/lazy-loading/src/App.js
+++ 2022/June_Batch/6Reactjs/Projects/lazy-loading/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter,Routes,Route, useSearchParams } from 'react-router-dom';
 import Header from './Header/Header';
 import Login from './Login/Login';
 import Register from './Register/Register';
@@ -11,10 +11,21 @@ const Product = React.lazy(()=>import('./Product/Product'));
 
 
 function App() {
+
+  const [getLoginStatus,setLoginStatus] = useState(true);
+
+  const onLoginHandler=()=>{
+    setLoginStatus(true);
+  }
+
+  const onLogoutHandler=()=>{
+    setLoginStatus(false);
+  }
+
   return (
    <div>
     <BrowserRouter>
-    <Header/>
+    <Header getLoginStatus={getLoginStatus}/>
      <Routes>
       <Route path="login" element={<Login/>}/>
       <Route path="register" element={<Register/>}/>
