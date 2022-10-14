52cd2ad2a6705d494b8d9a8811b376626b84a758
salman3009 <salman444g@gmail.com>
2022-10-14 21:28:11 +0530

    code

--- 2022/June_Batch/6Reactjs/Projects/redux-concept/src/App.js
+++ 2022/June_Batch/6Reactjs/Projects/redux-concept/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useDispatch, useSelector } from "react-redux";
-import {increment,decrement} from './redux/counter';
+import {increment,decrement,restart,userincrement} from './redux/counter';
 
 
 export default function App() {
@@ -16,6 +16,8 @@ export default function App() {
       <h1> The count is: {object.count}</h1>
       <button onClick={()=>dispatch(increment())}>Increment</button>
       <button onClick={()=>dispatch(decrement())}>Decrement</button>
+      <button onClick={()=>dispatch(restart())}>Restart</button>
+      <button onClick={()=>dispatch(userincrement(30))}>Increment By 30</button>
      
     </div>
   );
--- 2022/June_Batch/6Reactjs/Projects/redux-concept/src/redux/counter.js
+++ 2022/June_Batch/6Reactjs/Projects/redux-concept/src/redux/counter.js
@@ -12,11 +12,18 @@ export const counterSlice = createSlice({
     },
     decrement: (state) => {
       state.count -= 1;
+    },
+    restart:(state)=>{
+      state.count = 0;
+    },
+    userincrement:(state,action)=>{
+          console.log(action.payload);
+      state.count += action.payload;
     }
   }
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement} = counterSlice.actions;
+export const { increment, decrement,restart,userincrement} = counterSlice.actions;
 
 export default counterSlice.reducer;
