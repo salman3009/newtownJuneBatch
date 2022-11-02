1cd81877ef4c31ec90c7bffaa562753fc77e7661
salman3009 <salman444g@gmail.com>
2022-11-02 22:22:08 +0530

    code

--- 2022/June_Batch/7Nodejs/November/02-11-22/basic-express-setup/index.js
+++ 2022/June_Batch/7Nodejs/November/02-11-22/basic-express-setup/index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+
+
+app.get('',(req,res)=>{
+    res.send("hello world");
+});
+
+app.get('/about',(req,res)=>{
+    res.send("This is about page");
+});
+
+app.get('/contact',(req,res)=>{
+    res.send("<h1>Welcome to nodejs</h1><h2>Template engine</h2>");
+})
+
+
+
+app.listen(8080,()=>{
+    console.log("our application is listening on port 8080");
+});
